05b66b7ab5506f24462f139642c4a9466ce8f665
yuyuyu101 <haomaiwang@gmail.com>
2013-04-26 21:04:59 +0800

    Init version

---
+++ buffered_tree.h
@@ -0,0 +1,28 @@
+#ifndef WHEAT_BUFFERED_TREE
+#define WHEAT_BUFFERED_TREE
+
+#define BFTREE_DEFAULT_CONTAINER       10
+#define BFTREE_PAYLOAD_THRESHOLD       100
+#define BFTREE_CONTAINER_THRESHOLD     16
+
+#define BF_OK                          0
+#define BF_WRONG                       (-1)
+
+struct bftree;
+typedef int (*key_compare_func)(const void *key1, const void *key2);
+
+struct bftree_opts {
+    void *(*key_dup)(const void *key);
+    void *(*val_dup)(const void *obj);
+    key_compare_func key_compare;
+    void (*key_destructor)(void *key);
+    void (*val_destructor)(void *obj);
+};
+
+struct bftree *bftree_create(struct bftree_opts *opts);
+void bftree_free(struct bftree *tree);
+int bftree_put(struct bftree *tree, void *key, void *val);
+void *bftree_get(struct bftree *tree, void *key);
+int bftree_del(struct bftree *tree, void *key);
+
+#endif
